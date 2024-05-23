f6ad512e0e0b0477df7b5dea5b9b6343731dfb8d
Darshan <darshan.smartway@gmail.com>
2024-05-23 12:58:38 -0400

    Lable pacment change

--- tc.js
+++ tc.js
@@ -1,7 +1,21 @@
+var Clock = document.getElementById("sub");
+Clock.addEventListener("click", function () {
+   var st = document.getElementById("st").value;
+   console.log(st)
+   var et = document.getElementById("et").value;
+   console.log(et)
+//    var total = document.getElementById("total"); 
+//    console.dir(total)  
+   var t = et-st;
+   var la = document.getElementById("totaltvalue");
+   la.innerHTML= t;
+   console.log(t)
+//    console.log(total)
+//    total.textContent = t;
+}) 
 
-
-var Submit = document.getElementById("submit");
-Submit.addEventListener("click", function () {
+var Diff = document.getElementById("submit");
+Diff.addEventListener("click", function () {
     console.log("hi");
     var time = document.getElementById("fname").value;
     let h = time * 100 / 60;
@@ -9,25 +23,8 @@ Submit.addEventListener("click", function () {
     var p = document.createElement("p");
     var t = document.createTextNode(deci);
     p.appendChild(t);
-    document.getElementById("div").appendChild(p);      
+   // document.getElementById("div").appendChild(p);      
     document.getElementById("outputvalue").innerHTML=deci;
     console.log(h);
     console.log(deci);
-}) 
-
-var Sub = document.getElementById("sub");
-Sub.addEventListener("click", function () {
-   var st = document.getElementById("st").value;
-   console.log(st)
-   var et = document.getElementById("et").value;
-   console.log(et)
-   var total = document.getElementById("total"); 
-   console.dir(total)  
-   var t = et-st;
-   var la = document.getElementById("totaltvalue");
-   la.innerHTML= t;
-   console.log(t)
-   console.log(total)
-   total.textContent = t;
-}) 
-
+})
